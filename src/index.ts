b42a6072d0e90aa5da112d7ba51a4f365af170d7
官维 <guanwei@triumen.com>
2020-04-20 11:58:37 +0800

    可辨识联合类型

--- src/index.ts
+++ src/index.ts
@@ -395,3 +395,72 @@ type Container<T> = { value: T }
 // 类型别名看起来跟 interface 非常像，interface 只能用于定义对象类型，而 type 的声明方式除了对象之外还可以定义交叉、联合、原始类型等，类型声明的方式适用范围显然更加广泛。
 // 对于类型别名的使用： 能用 interface 就用 interface，不能就用 type
 
+// ------------------------------------- 可辨识联合类型
+// 字面量类型
+// 字面量（Literal Type）主要分为 真值字面量类型（boolean literal types）,数字字面量类型（numeric literal types）,枚举字面量类型（enum literal types）,大整数字面量类型（bigInt literal types）和字符串字面量类型（string literal types）。
+const af: 2333 = 2333 // ok
+const ab: 0b10 = 2 // ok
+const ao: 0o114 = 0b1001100 // ok
+const ax: 0x514 = 0x514 // ok
+const c: "xiaomuzhu" = "xiaomuzhu" // ok
+const d: false = false // ok
+// const gf: "github" = "pronhub" // 不能将类型“"pronhub"”分配给类型“"github"”
+
+type Direction = "North" | "East" | "South" | "West"
+function move(distance: number, direction: Direction) {}
+move(20, "East")
+
+// 类型字面量
+// 类型字面量(Type Literal)不同于字面量类型（Literal Type),跟 js 中的对象字面量的语法相似
+type Foo55 = {
+    baz: [number, "xiaomuzhu"]
+    toString(): string
+    readonly [Symbol.iterator]: "github"
+    0x1: "foo"
+    bar: 12n
+}
+
+// 可辨识联合类型
+interface Info {
+    username: string
+}
+type UserAction =
+    | {
+          action: "create"
+          info: Info
+      }
+    | {
+          action: "delete"
+          id: number
+          info: Info
+      }
+const userReducer = (userAction: UserAction) => {
+    switch (userAction.action) {
+        case "delete":
+            console.log(userAction.id)
+            break
+        default:
+            break
+    }
+}
+// userAction.action 就是辨识的关键, 被称为可辨识的标签
+
+//-------------------------------------- 装饰器
+// 装饰器就是在给一个函数拓展额外的功能，本质上也是一个函数
+// 类装饰器
+function addAge(constructor: Function) {
+    constructor.prototype.age = 18
+}
+@addAge
+class Person11 {
+    name: string
+    age!: string // ! 表示不可能为空值
+    constructor() {
+        this.name = "jack"
+    }
+}
+const person11 = new Person11()
+console.log(person11.age) // 18
+// 当装饰器作为修饰类的时候，会把构造器传递进去。 constructor.prototype.age 就是在每一个实例化对象上面添加一个 age 值
+
+// 属性/方法装饰似器
